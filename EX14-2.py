8309fac178b5d54f3e96647c4ef5bc97a870faad
GiGiGan <live10022001@gmail.com>
2017-10-05 18:07:28 +0800

    EX14-2～EX17

---
+++ EX14-2.py
@@ -0,0 +1,4 @@
+from collections import OrderedDict
+a = ["1", 1, "1", 2]
+a = list(OrderedDict.fromkeys(a))
+print(a)
---
+++ EX15-1.py
@@ -0,0 +1,2 @@
+#make a dictionary contain a and b and their value respective values 1 and 2
+d = {"a": 1, "b": 2}
